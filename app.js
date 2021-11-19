46bc0e640fcfbd834abb2ef12ece69354dfc099d
erictsaiu0 <knighterictsai@gmail.com>
2021-11-19 14:39:17 +0800

    1st publish

--- app.js
+++ app.js
@@ -96,3 +96,75 @@ form.addEventListener('submit', (e) => {
     checkLength(passwordConfirm, 8, 25);
     passwordMatch(password, passwordConfirm);
 })
+
+
+// image gallery
+let galleryImages = document.querySelectorAll('.services-cell');
+let getLatsestOpenedImg;
+let windowWidth = window.innerWidth;
+galleryImages.forEach(function(image, index){
+    image.onclick = function(){
+
+        getLatsestOpenedImg = index + 1;
+
+        let container = document.body;
+        let newImgWindow = document.createElement('div');
+        container.appendChild(newImgWindow);
+        newImgWindow.setAttribute('class', 'img-window');
+        newImgWindow.setAttribute('onclick', 'closeImg()');
+        
+        let newImg = image.firstElementChild.cloneNode();
+        newImgWindow.appendChild(newImg);
+        newImg.classList.remove('services-cell_img');
+        newImg.classList.add('popup-img');
+        newImg.setAttribute('id', 'current-img')
+
+        newImg.onload = function(){
+            let newNextBtn = document.createElement('a');
+            newNextBtn.innerHTML = '<i class="fas fa-chevron-right next"></i>';
+            container.appendChild(newNextBtn);
+            newNextBtn.setAttribute('class', 'img-btn-next');
+            newNextBtn.setAttribute('onclick', 'changeImg(1)');
+
+            let newPrevBtn = document.createElement('a');
+            newPrevBtn.innerHTML = '<i class="fas fa-chevron-left next"></i>';
+            container.appendChild(newPrevBtn);
+            newPrevBtn.setAttribute('class', 'img-btn-prev');
+            newPrevBtn.setAttribute('onclick', 'changeImg(0)');
+        }
+    }
+})
+
+function closeImg(){
+    document.querySelector('.img-window').remove();
+    document.querySelector('.img-btn-next').remove();
+    document.querySelector('.img-btn-prev').remove();
+}
+
+function changeImg(change){
+    document.querySelector('#current-img').remove();
+
+    let getImgWindow = document.querySelector('.img-window');
+    let newImg = document.createElement('img');
+    getImgWindow.appendChild(newImg);
+
+    let calcNewImg;
+    if(change == 1){
+        calcNewImg = getLatsestOpenedImg + 1;
+        if(calcNewImg > galleryImages.length){
+            calcNewImg = 1;
+        }
+    }
+    else if(change == 0){
+        calcNewImg = getLatsestOpenedImg - 1;
+        if(calcNewImg < 1){
+            calcNewImg = galleryImages.length;
+        }
+    }
+
+    newImg.setAttribute('src', 'gallery/A' + calcNewImg + '.jpg');
+    newImg.setAttribute('class', 'popup-img');
+    newImg.setAttribute('id', 'current-img');
+
+    getLatsestOpenedImg = calcNewImg;
+}
